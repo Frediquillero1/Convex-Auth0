da758c7f706edc0bdfa6881cb29725655f3c741f
Fred Emilio Schlesinger <37296162+Frediquillero1@users.noreply.github.com>
2024-08-27 16:45:20 +0200

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { UserButton } from '@/features/auth/components/user-button'
+import { useAuthActions } from '@convex-dev/auth/react'
+
+export default function Home() {
+  const { signOut } = useAuthActions()
+
+  return (
+    <div>
+      Logged in!&nbsp;
+       <UserButton />
+    </div>
+  )
+}
